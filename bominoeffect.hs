47c23cc77f2bcefcc063c01da4f86f55e2c83289
martijn.slot <martijn.slot@nedap.com>
2017-10-11 17:14:37 +0200

    puinhoop

--- bominoeffect.hs
+++ bominoeffect.hs
@@ -10,16 +10,16 @@ cls :: IO ()
 cls = putStr "\ESC[2J"
 
 width :: Int
-width = 8
+width = 2
 
 height :: Int
-height = 7
+height = 2
 
-inputboard :: [Int]
-inputboard = [5,4,3,6,5,3,4,6,0,6,0,1,2,3,1,1,3,2,6,5,0,4,2,0,5,3,6,2,3,2,0,6,4,0,4,1,0,0,4,1,5,2,2,4,4,1,6,5,5,5,3,6,1,2,3,1]
+-- inputboard :: [Int]
+-- inputboard = [5,4,3,6,5,3,4,6,0,6,0,1,2,3,1,1,3,2,6,5,0,4,2,0,5,3,6,2,3,2,0,6,4,0,4,1,0,0,4,1,5,2,2,4,4,1,6,5,5,5,3,6,1,2,3,1]
 
-resultboard :: [Int]
-resultboard = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+-- resultboard :: [Int]
+-- resultboard = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 bones :: [Bone]
 bones = [((0,0),1), ((1,1),8), ((2,3),15), ((3,6),22),
@@ -30,32 +30,24 @@ bones = [((0,0),1), ((1,1),8), ((2,3),15), ((3,6),22),
          ((0,5),6), ((1,6),13), ((3,4),20), ((5,6),27),
          ((0,6),7), ((2,2),14), ((3,5),21), ((6,6),28)]
 
-inputboard2 :: [Int]
-inputboard2 = [0,0,0,1]
+inputboard :: [Int]
+inputboard = [0,1,0,0]
 
-resultboard2 :: [Int]
-resultboard2 = [0,0,0,0]
-
-bones2 :: [Bone]
-bones2 = [bones !! 0, bones !! 5]
+resultboard :: [Int]
+resultboard = [0,0,0,0]
 
 findFree :: [Int] -> Int -- find next free spot on resultmatrix
 findFree = findFree' 0
 
 findFree' :: Int -> [Int] -> Int
-findFree' n (x:xs) | x == 0    = n
+findFree' n (x:xs) | xs == []  = (-1)
+                   | x == 0    = n
                    | otherwise = findFree' (n+1) xs
 
-findTrue :: [Bool] -> Int -- find next free spot on resultmatrix
-findTrue = findTrue' 0
-
-findTrue' :: Int -> [Bool] -> Int
-findTrue' n (x:xs) | x == True    = n
-                   | otherwise = findTrue' (n+1) xs
-
 findBoneValue :: (Int,Int) -> [Bone] -> Int --for a location on the board, find the corresponding bone value
+findBoneValue (a,b) []                                                          = 0
 findBoneValue (a,b) (((p, q), r):xs) | (a == p && b == q) || (a == q && b == p) = r
-                                   | otherwise                                = findBoneValue (a,b) xs
+                                     | otherwise                                = findBoneValue (a,b) xs
 
 matchBone :: (Int,Int) -> Bone -> Bool -- matches a position to a bone
 matchBone (a,b) ((p,q),_) = (a == p && b == q) || (a == q && b == p)
@@ -63,7 +55,7 @@ matchBone (a,b) ((p,q),_) = (a == p && b == q) || (a == q && b == p)
 matchBoneValue :: Int -> Bone -> Bool -- matches an int to a bone
 matchBoneValue n ((_,_),p) = n == p
 
-getValue :: (Int, Int) -> [Int] -> (Int, Int)
+getValue :: (Int, Int) -> [Int] -> (Int, Int) -- translate 'free locations' to values from inputboard
 getValue (a,b) xs = ((xs !! a), (xs !! b))
 
 getBoneValue :: Bone -> Int
@@ -87,14 +79,14 @@ chop n xs = take n xs : chop n (drop n xs)
 
 solve :: [Int] -> [Int] -> [Bone] -> [Int]
 solve inp out []                    = out
-solve inp out st  | boneValue1 /= 0 = solve inp (replace1) (removeBone boneValue1 st)
-                  | boneValue2 /= 0 = solve inp (replace2) (removeBone boneValue2 st)
-                  | otherwise       = []
+solve inp out st  | boneValue1 /= 0 && boneValue2 /= 0 = solve inp replace1 (removeBone boneValue1 st) ++ solve inp replace2 (removeBone boneValue2 st)
+                  | boneValue1 /= 0                    = solve inp replace1 (removeBone boneValue1 st)
+                  | otherwise                          = []
                   where
-                     replace1   = replaceBone (snd (freemoves !! 0)) boneValue1 (replaceBone (fst (freemoves !! 0)) boneValue1 out)
-                     replace2   = replaceBone (snd (freemoves !! 1)) boneValue2 (replaceBone (fst (freemoves !! 1)) boneValue2 out) 
-                     boneValue1 = findBoneValue (getValue (freemoves !! 0) inp) st
-                     boneValue2 = findBoneValue (getValue (freemoves !! 1) inp) st
+                     replace1   = replaceBone (snd (head freemoves)) boneValue1 (replaceBone (fst (head freemoves)) boneValue1 out)
+                     replace2   = replaceBone (snd (last freemoves)) boneValue2 (replaceBone (fst (last freemoves)) boneValue2 out) 
+                     boneValue1 = if length freemoves < 1 then 0 else findBoneValue (getValue (head freemoves) inp) st
+                     boneValue2 = if length freemoves < 2 then 0 else findBoneValue (getValue (last freemoves) inp) st
                      freemoves  = moves (findFree out) out
 
 --IO GEBEUREN
@@ -114,4 +106,4 @@ insert17CharNewLine xs | length xs < 8 = xs
 interleave :: a -> [a] -> [a]
 interleave x [] = []
 interleave x [y] = [y]
--- interleave x (y:ys) = y : x : interleave x ys
+interleave x (y:ys) = y : x : interleave x ys
