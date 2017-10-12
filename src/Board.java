78f82657d13d00f15c40b7c6d91f4abe90d86aa0
martijn.slot <martijn.slot@nedap.com>
2017-10-12 13:53:45 +0200

    field n board

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Board {
+class Board {
 
     private static final int WIDTH = 8;
     private static final int HEIGHT = 7;
-    private List<Field> fields = new ArrayList();
+    private List<Integer> fields = new ArrayList();
 
-    public Board (List<Field> fields) {
+    public Board (List<Integer> fields) {
         this.fields = fields;
+    }
+
+    public int getField(int a) {
+        return fields.get(a);
+    }
+
+    public Field adjustField (Field field, boolean filled) {
+        field.setFilled(filled);
+        return field;
     }
 }
