e30c0bd6ab900ca73e1b567b51a2e5fa5be3288e
martijn.slot <martijn.slot@nedap.com>
2017-10-13 09:10:41 +0200

    implemented methods as in haskell solver

--- src/Board.java
+++ src/Board.java
@@ -19,17 +19,40 @@ class Board {
         return fields.get(i);
     }
 
-    public void replaceFieldValue(Board board, int i, int value) {
-        board.getFields().set(i, value);
+    public void replaceValue(Board resultBoard, int i, int value) {
+        resultBoard.getFields().set(i, value);
     }
 
-    public int findFree (Board board) {
-        return board.getFields().indexOf(0);
+    public int findFree(Board resultBoard) {
+        return resultBoard.getFields().indexOf(0);
     }
 
-    private Board replaceBone (int i, int newValue, Board board) {
-        board.getFields().set(i, newValue);
-        return board;
+    public Board replaceBone(int i, int newValue, Board resultBoard) {
+        resultBoard.getFields().set(i, newValue);
+        return resultBoard;
+    }
+
+    public List<Position> moves(int i, Board resultBoard) {
+        List<Position> moves = new ArrayList<>();
+        if ((i + 1) % WIDTH == 0) {
+            moves.add(new Position(i, i + WIDTH));
+        } else if (i + WIDTH >= resultBoard.getFields().size()) {
+            moves.add(new Position(i, i + 1));
+        } else {
+            moves.add(new Position(i, i + WIDTH));
+            moves.add(new Position(i, i + 1));
+        }
+        moves = checkMoves(moves, resultBoard);
+        return moves;
+    }
+
+    private List<Position> checkMoves(List<Position> moves, Board resultBoard) {
+        for (int i = 0; i < moves.size(); i++) {
+            if (resultBoard.getValue(moves.get(i).getN1()) != 0 && resultBoard.getValue(moves.get(i).getN2()) != 0) {
+                moves.remove(i);
+            }
+        }
+        return moves;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Integer> input1 = Arrays.asList(4, 2, 5, 2, 6, 3, 5, 4, 5, 0, 4, 3, 1, 4, 1, 1, 1, 2, 3, 0, 2, 2, 2, 2, 1, 4, 0, 1, 3, 5, 6, 5, 4, 0, 6, 0, 3, 6, 6, 5, 4, 0, 1, 6, 4, 0, 3, 0, 6, 5, 3, 6, 2, 1, 5, 3);
+        List<Integer> input1 = Arrays.asList(4, 2, 5, 2, 6, 3, 5, 4, 5, 0, 4, 3, 1, 4, 1, 1, 1, 2, 3, 0, 2, 2, 2, 2, 1, 4, 0, 1, 3, 5, 6, 5, 4, 0, 6, 0, 3, 6, 6, 5, 4, 0, 1, 6, 4, 0, 3, 0, 6, 5, 3, 6, 2, 1, 5, 3); //make arraylist
         Board resultBoard1 = new Board(Arrays.asList(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
         List<Bone> bones = Arrays.asList(new Bone(0, 0, 1), new Bone(0, 1, 2), new Bone(0, 2, 3), new Bone(0, 3, 4), new Bone(0, 4, 5), new Bone(0, 5, 6),
             new Bone(0, 6, 7), new Bone(1, 1, 8), new Bone(1, 2, 9), new Bone(1, 3, 10), new Bone(1, 4, 11), new Bone(1, 5, 12), new Bone(1, 6, 13),
@@ -17,7 +17,11 @@ public class Main {
 
         Board finalBoard = Solver.solve(board1, resultBoard1, bones);
 
-        finalBoard.toString();
+        for (int i = 0; i < bones.size(); i++) {
+            bones.remove(i);
+        }
+
+//        finalBoard.toString();
     }
 
 }
